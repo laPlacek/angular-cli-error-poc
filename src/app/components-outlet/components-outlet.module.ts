62863753576036fbb518a514e761bb314e625a80
Jacek Krużel <jacek.kruzel@gmail.com>
2017-01-14 22:41:36 +0100

    simple

--- src/app/components-outlet/components-outlet.module.ts
+++ src/app/components-outlet/components-outlet.module.ts
@@ -2,36 +2,10 @@ import { NgModule, ModuleWithProviders, Provider } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ComponentsOutletComponent } from './components-outlet.component';
 import { COMPONENTS_OUTLET_CONFIG, ComponentOutletConfig } from './components-outlet-config';
-import { isArray } from 'lodash';
-
-export function provideComponentOutletConfig(componentOutletConfig?: ComponentOutletConfig | ComponentOutletConfig[]): Provider[] {
-  componentOutletConfig = componentOutletConfig || [];
-  if (!isArray(componentOutletConfig)) componentOutletConfig = [componentOutletConfig];
-
-  return componentOutletConfig.map(config => ({
-    provide: COMPONENTS_OUTLET_CONFIG,
-    multi: true,
-    useValue: componentOutletConfig
-  }));
-}
 
 @NgModule({
   imports: [CommonModule],
   declarations: [ComponentsOutletComponent],
   exports: [ComponentsOutletComponent]
 })
-export class ComponentsOutletModule {
-  static forRoot(componentOutletConfig?: ComponentOutletConfig | ComponentOutletConfig[]): ModuleWithProviders {
-    return {
-      ngModule: ComponentsOutletModule,
-      providers: [provideComponentOutletConfig(componentOutletConfig)]
-    };
-  }
-
-  static forChild(componentOutletConfig?: ComponentOutletConfig | ComponentOutletConfig[]): ModuleWithProviders {
-    return {
-      ngModule: ComponentsOutletModule,
-      providers: [provideComponentOutletConfig(componentOutletConfig)]
-    };
-  }
-}
+export class ComponentsOutletModule { }
